ed93850b60fddb43bb02aab818e8de7421f24866
Michele Schimd <skimmoz@gmail.com>
2021-08-06 16:05:08 +0200

    Added DP matrix and script components (with background logic)

--- programming/react/edit-react/src/edit.js
+++ programming/react/edit-react/src/edit.js
@@ -10,19 +10,124 @@ for (var j = 0; j < MAX_COLS; j++) {
     mat[0][j] = j;
 }
 
-var last_x = null;
-var last_y = null;
-
-function edit(x, y) {
+export function edit(x, y) {
+    console.log(`ED(${x},${y})`)
     let n = x.length;
     let m = y.length;
     for (var i = 1; i <= n; i++) {
         for (var j = 1; j <= m; j++) {
-            let delta = (x[i-1] == y[j-1]) ? 0 : 1;
-            mat[i][j] = Math.min(mat[i-1][j-1] + delta, mat[i-1][j]+1, mat[i][j-1]+1);
+            let delta = (x[i - 1] === y[j - 1]) ? 0 : 1;
+            mat[i][j] = Math.min(mat[i - 1][j - 1] + delta, mat[i - 1][j] + 1, mat[i][j - 1] + 1);
         }
     }
-    last_x = x;
-    last_y = y;
-    return mat[n][m];
+    return mat;
+}
+
+export function closeDiagonalBacktracking(dpMatrix, n, m) {
+    let i = n;
+    let j = m;
+    let script = [];
+    while (i > 0 && j > 0) {
+        let a = dpMatrix[i-1][j-1];
+        let b = dpMatrix[i-1][j];
+        let c = dpMatrix[i][j-1];
+        let d = dpMatrix[i][j];
+        if (a === d && a <= b && a <= c) { // Match
+            script.push([i,j,0]);
+            i--;
+            j--;
+            continue;
+        }
+        if (a < b && a < c) { // Sub
+			script.push([i, j, 1]);
+			i--;
+			j--;
+			continue;
+		}
+        if (b < a && b < c) { // Del
+			script.push([i, j, 1]);
+			i--;
+			continue;
+		}
+        if (c < a && c < b) { // Del
+			script.push([i, j, 1]);
+			j--;
+			continue;
+		}
+        if (a === b && b === c) { // Tie All
+			script.push([i, j, 1])
+			if (i < j) {
+				j--;
+			}
+			if (i === j) {
+				i--;
+				j--;
+			}
+			if (i > j) {
+				i--;
+			}
+			continue;
+		}
+        script.push([i, j, 1]);
+        if (a === b) {
+			if (i > j) {
+				i--;
+			} else {
+				i--;
+				j--;
+			}
+		}
+        if (a === c) {
+			if (i < j) {
+				j--;
+			} else {
+				i--;
+				j--;
+			}
+		}
+        if (b === c) {
+			if (i < j) {
+				j--;
+			} else {
+				i--;
+			}
+		}
+    }
+    while(i > 0) {
+		script.push([i, j, 1]);
+		i--;
+	}
+	while(j > 0) {
+		script.push([i, j, 1]);
+		j--;
+	}
+	script.push([0, 0, 0]);
+	return script.reverse();
+}
+
+export function scriptToOps(script) {
+    let k = script.length;
+    let ops = []
+    for (let i = 1; i < k; i++) {
+        if (script[i][2] === 0) {
+            ops.push('M');
+            continue;
+        }
+        let di = script[i][0] - script[i-1][0];
+        let dj = script[i][1] - script[i-1][1];
+        if (di > 0 && dj > 0) {
+            ops.push('S');
+            continue;
+        }
+        if (di === 0) {
+            ops.push('S');
+            continue
+        }
+        if (dj === 0) {
+            ops.push('D');
+            continue;
+        }
+        ops.push('?');
+    }
+    return ops;
 }
--- programming/react/edit-react/src/index.js
+++ programming/react/edit-react/src/index.js
@@ -1,39 +1,153 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { edit, closeDiagonalBacktracking, scriptToOps } from './edit.js';
 
-const StringInput = (props) => {
-  return (
-    <div>
-      <label className="formLabel">{props.label}</label>
-      <input type="text" />
-      <button onClick={f=>f}>Random</button>
-      <br />
-    </div>
-  )
+
+class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: "ACGT",
+      y: "ACG",
+    }
+    this.handleChangeX = this.handleChangeX.bind(this);
+    this.handleChangeY = this.handleChangeY.bind(this);
+    this.compute = this.compute.bind(this);
+  }
+
+  compute() {
+    this.props.computeCallback(this.state.x, this.state.y);
+  }
+
+  handleChangeX(e) {
+    this.setState({ x: e.target.value });
+  }
+
+  handleChangeY(e) {
+    this.setState({ y: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="formHeading2">Input</h2>
+        <div>
+          <label className="formLabel">x</label>
+          <input type="text" value={this.state.x} onChange={this.handleChangeX} />
+          <button onClick={f => f}>Random</button>
+          <br />
+        </div>
+        <div>
+          <label className="formLabel">y</label>
+          <input type="text" value={this.state.y} onChange={this.handleChangeY} />
+          <button onClick={f => f}>Random</button>
+          <br />
+        </div>
+        <button onClick={this.compute}>Go!</button>
+      </div>
+    );
+  }
 }
 
-const InputForm = () => {
+const EditInfo = (props) => {
   return (
-    <div>
-      <h2 className="formHeading2">Input</h2>
-      <StringInput label="x" />
-      <StringInput label="y" />
-      <button onClick={f=>f}>Go!</button>
+    <div className="editInfoDiv">
+      <p>Edit Distance: {props.distance}</p>
     </div>
   );
 }
 
-const App = () => {
+const DPMatrix = (props) => {
   return (
-    <div>
-      <h1 id="heading">Edit Distance Playground</h1>
-      <InputForm />
+    (!props.matrix) ?
+    <div></div> :
+    <div className="dpMatrixDiv">
+      <table className="dpMatrixTab"><tbody>
+        
+        <tr key="(-1,*)">
+          <td key="(-1,-1)"></td>
+          <td key="(0,-1)"></td>
+          {[...props.y].map((c,j) =>
+            <td key={`(0,${j+1})`}className="dpMatrixTD">{c}</td>
+          )}
+        </tr>
+
+        <tr key="(0,*)">
+          <td key="(-1,0)"></td><td key="(0,0)"className="dpMatrixTD">{0}</td>
+          {[...props.y].map((c,i) => <td key={`(0,${i+1})`} className="dpMatrixTD">{i+1}</td>)}
+        </tr>
+
+        {[...props.x].map((c, i) =>
+          <tr key={`(${i+1},*)`}>  
+            <td key={`(${i+1},0)`} className="dpMatrixTD">{c}</td>
+            {props.matrix[i+1].map((m,j) => <td key={`(${i+1},${j+1})`} className="dpMatrixTD">{m}</td>)}
+          </tr>
+        )}
+      </tbody></table>
     </div>
   );
+}
+
+const EditScript = ({x, y, matrix}) => {
+  return (
+    <div className="scriptDiv">
+      {(matrix) ?
+        scriptToOps(closeDiagonalBacktracking(matrix, x.length, y.length))
+        :
+        ""}
+    </div>
+  );
+}
+
+
+
+const Output = (props) => {
+  return (
+    <div>
+      <h2 className="formHeading2">Output</h2>
+      <EditInfo distance={props.distance} matrix={props.matrix} />
+      <DPMatrix x={props.x} y={props.y} matrix={props.matrix} />
+      <EditScript x={props.x} y={props.y} matrix={props.matrix} />
+    </div>
+  );
+}
+
+class Playground extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: "",
+      y: "",
+      distance: 0,
+      matrix: null,
+    }
+    this.compute = this.compute.bind(this);
+  }
+
+  compute(x, y) {
+    let mat = edit(x, y)
+    let d = mat[x.length][y.length]
+    this.setState({ x: x, y: y, distance: d, matrix: mat });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="heading">Edit Distance Playground</h1>
+        <InputForm computeCallback={this.compute} />
+        <Output
+          x={this.state.x}
+          y={this.state.y}
+          distance={this.state.distance}
+          matrix={this.state.matrix}
+        />
+      </div >
+    );
+  }
 }
 
 ReactDOM.render(
-  <App />,
+  <Playground />,
   document.getElementById('root')
 );
